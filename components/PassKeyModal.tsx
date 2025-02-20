22df508ef08752f71db2d6320108b09e8e6d46c3
joab <joabrb@yahoo.com>
2025-02-20 17:40:32 -0300

    hoje

---
+++ components/PassKeyModal.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const PassKeyModal = () => {
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(true);
+  const [passKey, setPassKey] = useState("");
+  const [error, setError] = useState("");
+
+  const closeModal = () => {
+    setIsOpen(false);
+    router.push("/");
+  };
+
+  return (
+    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+      <AlertDialogContent className="shad-alert-dialog">
+        <AlertDialogHeader>
+          <AlertDialogTitle className="flex items-start justify-between">
+            Admin Access Verification
+            <Image
+              src="/assets/icons/close.svg"
+              alt="close"
+              width={20}
+              height={20}
+              onClick={() => closeModal()}
+              className="cursor-pointer"
+            />
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            To access the admi page, please enter the passkey.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <div>
+          <InputOTP maxLength={6} value={passKey} onChange={(value) => setPassKey(value)}>
+            <InputOTPGroup className="shad-otp">
+              <InputOTPSlot className="shad-otp-slot" index={0} />
+              <InputOTPSlot className="shad-otp-slot" index={1} />
+              <InputOTPSlot className="shad-otp-slot" index={2} />
+              <InputOTPSlot className="shad-otp-slot" index={3} />
+              <InputOTPSlot className="shad-otp-slot" index={4} />
+              <InputOTPSlot className="shad-otp-slot" index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+          {error && <p className="shad-error text-14-regular mt-4 flex justify-center">
+                {error}
+            </p>}
+        </div>
+        <AlertDialogFooter>
+          <AlertDialogAction onClick={(e) => validatePasskey(e)}
+            className="shad-primary-btn w-full"
+            >
+            Enter Admin Passkey
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default PassKeyModal;
--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -8,7 +8,10 @@ import { Form } from "@/components/ui/form";
 import CustomFormField from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
 import { useState } from "react";
-import { AppointmentFormValidation, getAppointmentSchema } from "@/lib/validation";
+import {
+  AppointmentFormValidation,
+  getAppointmentSchema,
+} from "@/lib/validation";
 import { useRouter } from "next/navigation";
 import { createUser } from "@/lib/actions/patient.actions";
 import { FormFieldType } from "./PatientForm";
@@ -35,60 +38,58 @@ const AppointmentForm = ({
   const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
-    primaryPhysician: "",
-    schedule: new Date(),
-    reason: "",
-    note: "",
-    cancellationReason: "",
-   },
+      primaryPhysician: "",
+      schedule: new Date(),
+      reason: "",
+      note: "",
+      cancellationReason: "",
+    },
   });
 
   async function onSubmit(values: z.infer<typeof AppointmentFormValidation>) {
-    setIsLoading(true); 
+    setIsLoading(true);
 
     let status;
 
     switch (type) {
-        case 'schedule':
-            status = "scheduled";
-            break;
+      case "schedule":
+        status = "scheduled";
+        break;
 
-        case 'cancel':
-            status = "cancelled";
-            break;
-        default:
-            status = "pending";
-            break;
+      case "cancel":
+        status = "cancelled";
+        break;
+      default:
+        status = "pending";
+        break;
     }
 
     console.log("BEFORE THE TYPE", type);
-    
 
     try {
-        if(type === 'create' && patientId) {
-            console.log('IM HERE');
-            
-            const appointmentData = {
-                userId,
-                patient: patientId,
-                primaryPhysician: values.primaryPhysician,
-                schedule: new Date(values.schedule),
-                reason: values.reason!,
-                note: values.note,
-                status: status as Status,
+      if (type === "create" && patientId) {
+        console.log("IM HERE");
 
-            }
-            const appointment = await createAppointment(appointmentData)
+        const appointmentData = {
+          userId,
+          patient: patientId,
+          primaryPhysician: values.primaryPhysician,
+          schedule: new Date(values.schedule),
+          reason: values.reason!,
+          note: values.note,
+          status: status as Status,
+        };
+        const appointment = await createAppointment(appointmentData);
 
-            console.log(appointment);
+        console.log(appointment);
 
-            if(appointment) {
-                form.reset();
-                router.push(`/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`);
-            }
+        if (appointment) {
+          form.reset();
+          router.push(
+            `/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`
+          );
         }
-
-
+      }
     } catch (error) {
       console.log(error);
     }
@@ -106,9 +107,9 @@ const AppointmentForm = ({
       buttonLabel = "Create Appointment";
       break;
 
-    case 'schedule':
-        buttonLabel = "Schedule Appointment";
-        break;  
+    case "schedule":
+      buttonLabel = "Schedule Appointment";
+      break;
 
     default:
       break;
@@ -197,8 +198,7 @@ const AppointmentForm = ({
             type === "cancel" ? "shad-danger-btn" : "shad-primary-btn"
           } w-full`}
         >
-            {buttonLabel}
-
+          {buttonLabel}
         </SubmitButton>
       </form>
     </Form>
