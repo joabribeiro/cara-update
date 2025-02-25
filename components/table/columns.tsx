829975e1fccde61712acd20e32da7681a57350dc
joab <joabrb@yahoo.com>
2025-02-25 17:06:18 -0300

    feat: implement appointment modal and integrate with table actions

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -1,26 +1,16 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
-import {  MoreHorizontal } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
-import { Appointment } from "@/types/appwrite.types";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {StatusBadge} from "../StatusBadge";
+import { StatusBadge } from "../StatusBadge";
 import { formatDateTime } from "@/lib/utils";
 import { Doctors } from "@/constants";
 import Image from "next/image";
+import AppointmentModal from "../AppointmentModal";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type Payment = {
+  primaryPhysician: string;
   patient: any;
   id: string;
   amount: number;
@@ -32,64 +22,66 @@ export const columns: ColumnDef<Payment>[] = [
   {
     header: "ID",
     cell: ({ row }) => <p className="text-14-medium">{row.index + 1}</p>,
-  }, 
+  },
 
   {
     accessorKey: "patient",
-    header: "Patient", 
-    cell: ({row}) => <p className="text-14-medium">{row.original.patient.name}</p>
-    
+    header: "Patient",
+    cell: ({ row }) => (
+      <p className="text-14-medium">{row.original.patient.name}</p>
+    ),
   },
 
   {
     accessorKey: "status",
     header: "Status",
     cell: ({ row }) => (
-        <div className="min-w-[115px]">
-            <StatusBadge status={row.original.status}/>
-        </div>
-    )
+      <div className="min-w-[115px]">
+        <StatusBadge status={row.original.status} />
+      </div>
+    ),
   },
   {
     accessorKey: "schedule",
     header: "Appointment",
-    cell: ({row}) => (
+    cell: ({ row }) => (
       <p className="text-14-regular min-w-[100px]">
         {formatDateTime(row.original.schedule).dateTime}
       </p>
-    )
+    ),
   },
   {
     accessorKey: "primaryPhysician",
-    header: () => 'Doctor',
+    header: () => "Doctor",
     cell: ({ row }) => {
-     const doctor = Doctors.find((doc) => doc.name === row.original.primaryPhysician);
+      const doctor = Doctors.find(
+        (doc) => doc.name === row.original.primaryPhysician
+      );
 
-     return (
-      <div className="flex items-center gap-3">
-        <Image
-        src={doctor?.image}
-        alt={doctor?.name}
-        width={100}
-        height={100}
-        className="size-8"
-        />
-        <p className="whitespace-nowrap">
-          Dr. {doctor?.name}
-        </p>
-      </div>
-     )
+      return (
+        <div className="flex items-center gap-3">
+          <Image
+            src={doctor?.image}
+            alt={doctor?.name}
+            width={100}
+            height={100}
+            className="size-8"
+          />
+          <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+        </div>
+      );
     },
   },
   {
     id: "actions",
-    header: () => <div className="pl-4">Actions</div>
+    header: () => <div className="pl-4">Actions</div>,
     cell: ({ row }) => {
       return (
-        <div className="">
-
+        <div className="flex gap-1">
+          <AppointmentModal type="schedule" />
+          <AppointmentModal type="cancel" />
         </div>
-      )
+      );
     },
   },
 ];
